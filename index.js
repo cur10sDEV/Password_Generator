7e9026a8993db035dd5867f93690b5f48d48cc67
specsx-codebot <specsx@protonmail.com>
2021-10-30 17:21:06 +0530

    completed indexjs(entry) file

---
+++ index.js
@@ -0,0 +1,18 @@
+const program = require("commander");
+
+program.version("1.0.0").description("Simple Password Generator");
+
+program
+    .option("-l, --length <number>", "length of password","8")
+    .option("-s, --save", "save password to passwords.txt")
+    .option("-nn, --no-numbers", "remove numbers")
+    .option("-ns, --no-symbols", "remove symbols")
+    .parse();
+
+const {length, save, numbers, symbols} = program.opts()
+
+//Get generated password
+const generatedPassword = createPassword(length, numbers, symbols)
+
+//Output generated Passwoord
+console.log(generatedPassword);
